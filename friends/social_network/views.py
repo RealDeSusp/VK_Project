4800d77128f016bb87e126769a1afbff013900b0
Michael <thesuspect182@gmail.com>
2023-05-08 17:01:09 +0300

    added register

--- friends/social_network/views.py
+++ friends/social_network/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
 
 
 def index(request):
     return HttpResponse("Hello")
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = RegistrationForm()
+    return render(request, 'registration/register.html', {'form': form})
